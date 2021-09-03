e9ba6b95e0ab86851f6a7547a58bee0c1175a390
Larry Dennis <ronin@roninsa.com>
2021-09-02 20:01:48 -0700

    a bunch of files added and edited

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: "My Angry Poodle Site",
+    title: "My First Gatsby Site",
   },
   plugins: [
-    "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
